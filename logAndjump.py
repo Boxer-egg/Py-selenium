88052886ffe40e22016a063ed54f0d5c56849915
Boxer-egg <tarsqing@gmail.com>
2023-08-08 22:55:17 +0800

    将登录写成方法供调用

--- logAndjump.py
+++ logAndjump.py
@@ -1,7 +1,9 @@
 import time
+import yaml
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 with open('config.yaml') as f:
